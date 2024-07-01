70fa376cef1b3c5434e1d06384356a027a4008f8
Tomislav Dukez <tomdu3@ymail.com>
2024-07-01 08:00:15 +0200

    add dynamic jinja elements

--- Quest2/app/app.py
+++ Quest2/app/app.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, render_template
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
+username = "Tomdu3"
+description = "I love developing web applications."
+description += "And this text was created with Jinja"
+
+
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('index.html', username = username, description = description)
 
 @app.route('/about')
 def about():
