6f46affb07bf01df26a7094807c3beaa29850363
Tomislav Dukez <tomdu3@ymail.com>
2024-07-01 07:58:30 +0200

    update root route

--- Quest2/app/app.py
+++ Quest2/app/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
