f86aeaf1224c458e035938a97c4f06288c1fcfed
Tomislav Dukez <tomdu3@ymail.com>
2024-07-01 07:51:48 +0200

    add about and contact routes

--- Quest2/app/app.py
+++ Quest2/app/app.py
@@ -5,5 +5,17 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/about')
+def about():
+    return 'About Page'
+
+@app.route('/contact', methods = ['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        return 'You are using POST'
+    elif request.method == 'GET':
+        return 'You are using GET'
+
+
 if __name__ == '__main__':
     app.run()
