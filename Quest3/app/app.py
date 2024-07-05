7c3e5f515f168c758f0b2307f96c220621e7dd1e
Tomislav Dukez <tomdu3@ymail.com>
2024-07-05 10:18:56 +0200

    add login manager

--- Quest3/app/app.py
+++ Quest3/app/app.py
@@ -12,3 +12,12 @@ app = Flask(__name__, template_folder = 'templates', static_folder = 'static')
 app.config['SECRET_KEY'] = secrets.token_hex(16)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
